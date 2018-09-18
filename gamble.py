f0ee93c4904f932e281f4d8f0c6312ea05c5e723
Jordan Armstrong <jordan.armstrong3@stu.jefferson.kyschools.us>
2018-09-18 08:57:11 -0400

    added this feature

---
+++ gamble.py
@@ -0,0 +1,10 @@
+def adoption(name,age,dogs_owned,children,residental):
+    buyer = {
+        'name':name,
+        'age':age,
+        'dogs_owned':dogs_owned,
+        'children':children,
+        'residental':residental,
+        
+        }
+    return buyer
